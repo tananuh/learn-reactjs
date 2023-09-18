c7b8ed623580bac7719509837047cf57db1137ce
trunght-asia <trunght@arrowhitech.com>
2023-09-19 02:47:13 +0700

    todo state

--- src/features/Todo/components/TodoList/index.jsx
+++ src/features/Todo/components/TodoList/index.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import './styles.scss';
 
 TodoList.propTypes = {
     todoList: PropTypes.array,
+    onTodoClick: PropTypes.func,
 };
 
 TodoList.defaultProps = {
     todoList: [],
+    onTodoClick: null ,
 }
 
-function TodoList({todoList}) {
+function TodoList({todoList, onTodoClick}) {
+    const handleTodoClick = (todo, idx) => {
+        if(!onTodoClick) return;
+        onTodoClick(todo, idx);
+    }
     return (
-        <ul>
-            {todoList.map(todo => (
-                <li key={todo.id}>
-                    {todo.title}
+        <ul className ='todo-list'>
+            {todoList.map( (todo,idx) => (
+                <li key={todo.id} 
+                className={classnames({
+                    completed: todo.status === 'completed',
+                    'todo-item': true
+                })}
+                onClick={() => handleTodoClick(todo, idx)}>
+                    {todo.code}
                 </li>
             ))}
         </ul>
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from 'react'
 import PropTypes from 'prop-types';
 import TodoList from './components/TodoList';
 TodoFeature.propTypes = {
@@ -6,24 +7,42 @@ TodoFeature.propTypes = {
 };
 
 function TodoFeature(props) {
-    const todoList =[
+    const initTodoList =[
         {
             id: 1,
-            title: 'eat'
+            code: 'eat',
+            status: 'new'
         },{
             id: 2,
-            title: 'sleep'
+            code: 'sleep',
+            status: 'completed'
         },{
             id: 3,
-            title: 'code'
+            code: 'code',
+            status: 'new'
         },
-    ]
-    return (
+    ];
+
+    const [todoList, setTodoList] = useState(initTodoList);
+
+    const handleTodoClick = (todo, idx) => {
+        const newTodoList = [...todoList]
+        console.log(todo, idx);
+
+        newTodoList[idx] = {
+            ...newTodoList[idx],
+            status: newTodoList[idx].status === 'new' ?'completed':'new'
+        };
+
+        setTodoList(newTodoList);
+    }
+
+    return(
         <div>
             <h3>
                 Todo List
             </h3>
-            <TodoList todoList ={todoList}/>
+            <TodoList todoList ={todoList} onTodoClick={handleTodoClick} />
         </div>
     );
 }
