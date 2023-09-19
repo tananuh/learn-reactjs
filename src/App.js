273ae6ea4e2181dbf7f359bf844b966ff1aef577
trunght-asia <trunght@arrowhitech.com>
2023-09-20 03:44:21 +0700

    link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {
+  NavLink,
   Routes,
   Route
 } from "react-router-dom";
@@ -10,6 +11,10 @@ function App() {
   return (
     <div className="App">
       Homepage
+      <br></br>
+      <NavLink to='todos' className={(navData) => (navData.isActive ? "current-page" : 'none')}>Todo</NavLink>
+      <br></br>
+      <NavLink to='albums' className={(navData) => (navData.isActive ? "current-page" : 'none')}>Album</NavLink>
       <Routes>
         <Route path="/todos" element={<TodoFeature />} />
         <Route path="/albums" element={<AlbumFeature />} />
