4d0c4ad68a6a851026f494570f294eb012a35511
trunght-asia <trunght@arrowhitech.com>
2023-09-20 03:29:38 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
 import AlbumFeature from './features/Album';
+import TodoFeature from './features/Todo';
 
 function App() {
   return (
     <div className="App">
-      <AlbumFeature/>
+      Homepage
+      <Routes>
+        <Route path="/todos" element={<TodoFeature />} />
+        <Route path="/albums" element={<AlbumFeature />} />
+      </Routes>
+      
     </div>
   );
 }
